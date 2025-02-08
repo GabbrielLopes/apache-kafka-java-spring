77c2b6c48c671e045075c05c5a586741773235f3
GabbrielLps <jhosegabriel95@gmail.com>
2025-02-08 20:09:17 -0300

    init commit - setup

---
+++ src/main/java/dev/gabbriellps/kakfa/api/KakfaApiApplication.java
@@ -0,0 +1,13 @@
+package dev.gabbriellps.kakfa.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakfaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakfaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gabbriellps/kakfa/api/KakfaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gabbriellps.kakfa.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakfaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
