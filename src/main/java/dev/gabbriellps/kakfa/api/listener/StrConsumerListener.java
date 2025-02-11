667e4f37309d7a23e1d82f0e61d675e4835d8257
GabbrielLps <jhosegabriel95@gmail.com>
2025-02-11 00:00:42 -0300

    feat: seta particao para consumer especifico
    feat: seta particao para consumer especifico

--- src/main/java/dev/gabbriellps/kakfa/api/listener/StrConsumerListener.java
+++ src/main/java/dev/gabbriellps/kakfa/api/listener/StrConsumerListener.java
@@ -2,6 +2,7 @@ package dev.gabbriellps.kakfa.api.listener;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -9,12 +10,18 @@ import org.springframework.stereotype.Component;
 public class StrConsumerListener {
 
 
-    @KafkaListener(groupId = "group-1", topics = "first-topic", containerFactory = "strContainerFactory")
+    @KafkaListener(groupId = "group-1",
+            topicPartitions = {
+                @TopicPartition(topic = "first-topic", partitions =  {"0"})
+            }, containerFactory = "strContainerFactory")
     public void listener1(String message) {
         log.info("Listener 1 ::: group 1");
     }
 
-    @KafkaListener(groupId = "group-1", topics = "first-topic", containerFactory = "strContainerFactory")
+    @KafkaListener(groupId = "group-1",
+            topicPartitions = {
+            @TopicPartition(topic = "first-topic", partitions =  {"1"})
+    }, containerFactory = "strContainerFactory")
     public void listener2(String message) {
         log.info("Listener 2 ::: group 1");
     }
