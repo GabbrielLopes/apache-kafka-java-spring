1ee155ab7f0a55f38c276ec13ab4e2076292ad5a
GabbrielLps <jhosegabriel95@gmail.com>
2025-02-13 19:18:19 -0300

    feat: tratamento de exception personalizado
    feat: tratamento de exception personalizado

---
+++ src/main/java/dev/gabbriellps/kakfa/api/exception/ErrorCustomHandler.java
@@ -0,0 +1,30 @@
+package dev.gabbriellps.kakfa.api.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.springframework.kafka.listener.KafkaListenerErrorHandler;
+import org.springframework.kafka.listener.ListenerExecutionFailedException;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class ErrorCustomHandler implements KafkaListenerErrorHandler {
+
+
+    @Override
+    public Object handleError(Message<?> message, ListenerExecutionFailedException e) {
+        log.info("EXCEPTION HANDLER ::: Capturei um erro");
+        log.info("Payload ::: {}", message.getPayload());
+        log.info("Headers ::: {}", message.getHeaders());
+        log.info("Offset ::: {}", message.getHeaders().get("kafka_offset"));
+        log.info("Exception Message ::: {}", e.getMessage());
+        return null;
+    }
+
+//    @Override
+//    public Object handleError(Message<?> message, ListenerExecutionFailedException exception, Consumer<?, ?> consumer) {
+//        return KafkaListenerErrorHandler.super.handleError(message, exception, consumer);
+//    }
+
+}
